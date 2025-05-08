1f6730a2588e708264ac7494f02cee5e811aeaa4
n9hquan <quan.nguyen.220066@student.fulbright.edu.vn>
2025-05-09 00:05:54 +0700

    Remove /news and /search routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,8 @@
-const newsRouter = require('./news');
 const meRouter = require('./me');
 const siteRouter = require('./site');
 const coursesRouter = require('./courses')
 
 function route(app) {
-    app.use('/news', newsRouter);
 
     app.use('/me', meRouter);
 
