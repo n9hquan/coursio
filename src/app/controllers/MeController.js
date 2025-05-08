8bc9c0995e63fa05d5eac9b92cf22c9762bc8b26
n9hquan <quan.nguyen.220066@student.fulbright.edu.vn>
2025-05-08 10:17:17 +0700

    Feature: Add Select All with checkbox

---
+++ src/app/controllers/MeController.js
@@ -0,0 +1,26 @@
+const Course = require('../models/Course')
+const { multipleMongooseToObject } = require('../../util/mongoose')
+
+class MeController {
+    // [GET] /me/stored/courses
+    storedCourses(req, res, next) {
+        Promise.all([Course.find({}).lean(), Course.countDocumentsWithDeleted({ deleted: true })])
+            .then(([courses, deletedCount]) =>
+                res.render('me/stored-courses', {
+                    courses,
+                    deletedCount,
+                }))
+            .catch(next)
+    }
+
+    // [GET] /me/trash/courses
+    trashCourses(req, res, next){
+        Course.findDeleted({ })
+            .then(courses => res.render('me/trash-courses', {
+                courses: multipleMongooseToObject(courses)
+            }))
+            .catch(next)
+    }
+}
+
+module.exports = new MeController();
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const morgan = require('morgan');
 const sass = require('sass');
 const path = require('path');
+const methodOverride = require('method-override');
 const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
 const route = require('./routes');
+
+const db = require('./config/db');
+
+// Connect to DB
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
@@ -16,20 +22,25 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'))
+
 app.use(morgan('combined'));
 
 app.engine(
     'hbs',
     engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        }
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', './src/resources/views');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
